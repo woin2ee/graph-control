0a1b524e0221d0b2e6556b987c0c8bb3866e175f
Jaewon <woin2ee@gmail.com>
2022-07-18 16:06:09 +0900

    feat : Configure default UI

---
+++ GraphControl/MainScene/View/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  GraphControl
+//
+//  Created by Jaewon on 2022/07/18.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+    
+    @IBOutlet weak var sliderBar: UISlider!
+    
+    @IBOutlet weak var graphHeightConstraint: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
