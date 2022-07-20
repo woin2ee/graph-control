eceb2c852f59f6b41d535d01c256048eda62bd76
Jaewon <woin2ee@gmail.com>
2022-07-20 19:38:08 +0900

    feat : RxSwift 이용해서 ViewModel 구현

--- GraphControl/Scenes/Main/ViewModel/MainViewModel.swift
+++ GraphControl/Scenes/Main/ViewModel/MainViewModel.swift
@@ -6,18 +6,36 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
 final class MainViewModel: ViewModelType {
     
+    private let disposeBag = DisposeBag.init()
+    private let initialValue: Double = 50
+    
     struct Input {
-        
+        var didChangeValue: Driver<Float>
     }
     
     struct Output {
-        
+        var graph: Driver<Graph>
     }
     
+    // Input 을 Output 으로 변환
     func transform(input: Input) -> Output {
+        let observableGraph: Observable<Graph> = .of(Graph.init(value: initialValue))
         
+        let graph = input.didChangeValue
+            .withLatestFrom(
+                observableGraph.asDriver(onErrorJustReturn: Graph.init(value: initialValue))
+            )
+            .do( // Debug
+                onNext: { graph in
+                    print(graph.value)
+                }
+            )
+            
+        return Output.init(graph: graph)
     }
 }
