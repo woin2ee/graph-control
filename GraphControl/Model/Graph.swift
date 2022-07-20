6f39c956ea512a5fa46d12ce7dc684494655bbe8
Jaewon <woin2ee@gmail.com>
2022-07-20 17:59:27 +0900

    feat : Add Graph Model

---
+++ GraphControl/Model/Graph.swift
@@ -0,0 +1,14 @@
+//
+//  Graph.swift
+//  GraphControl
+//
+//  Created by Jaewon on 2022/07/18.
+//
+
+import Foundation
+
+struct Graph {
+    let minValue = 0
+    let maxValue = 100
+    var currentValue: Double
+}
